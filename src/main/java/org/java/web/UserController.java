58b7860375338ed864ce2a9181e9c9406aa4a254
1205552891 <1205552891@qq.com>
2019-08-05 08:07:59 +0800

    111

---
+++ src/main/java/org/java/web/UserController.java
@@ -0,0 +1,13 @@
+package org.java.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author:程聪
+ * @Date:2019/8/5 0005 08 06
+ * @Description
+ */
+@Controller
+public class UserController {
+
+}
